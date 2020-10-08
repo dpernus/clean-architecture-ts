9215c2a8817ee0faf22f6bc2dbedb297e0e1c850
dpernus <dpernus@gmail.com>
2020-10-08 16:09:59 -0300

    Update mongoose subschemas with id false

--- src/infraestructure/repositories/mongo/userModel.ts
+++ src/infraestructure/repositories/mongo/userModel.ts
@@ -1,58 +1,70 @@
 import { Document, Schema, model } from 'mongoose'
 import { Job, Course, Skills } from '../../../domain/entities/user'
 
-const personalDataSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const personalDataSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    address: String,
+    phone: Number,
+    social: String,
   },
-  age: {
-    type: Number,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  address: String,
-  phone: Number,
-  social: String,
-})
+  { _id: false },
+)
 
-const InstitutionSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const InstitutionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    web: String,
   },
-  description: String,
-  web: String,
-})
+  { _id: false },
+)
 
-const JobSchema = new Schema({
-  date: {
-    type: String,
-    required: true,
+const JobSchema = new Schema(
+  {
+    date: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    institution: InstitutionSchema,
+    achivements: [String],
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  institution: InstitutionSchema,
-  achivements: [String],
-})
+  { _id: false },
+)
 
-const CourseSchema = new Schema({
-  date: {
-    type: String,
-    required: true,
+const CourseSchema = new Schema(
+  {
+    date: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    institution: InstitutionSchema,
+    description: [String],
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  institution: InstitutionSchema,
-  description: [String],
-})
+  { _id: false },
+)
 
 const UserSchema = new Schema({
   id: {
